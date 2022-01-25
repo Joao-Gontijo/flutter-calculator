24fe945f46435ec94898dc02e721b77cf6e6899d
Joao-Gontijo <jvgontijo166@gmail.com>
2022-01-25 10:13:08 -0300

    starting memory operations

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,2 +1,20 @@
 //classe responsável por mostrar os dados no display
-class Memory {}
+class Memory {
+  String _value = '0';
+
+  String get value {
+    return _value;
+  }
+
+  void applyCommand(String command) {
+    if (command == 'AC') {
+      _allClear();
+    } else {
+      _value += command; //valor sempre vai ter o valor novo
+    }
+  }
+
+  _allClear() {
+    _value = '0';
+  }
+}
